b7c9f7fdbdd4cca02c77641dbb819106db0767e3
Jorge Washington Soares da Silva <jwss486@gmail.com>
2026-08-22 10:06:58 -0300

    Esqueleto do Projeto

---
+++ src/test/java/com/jwss/studios/loja_virtual_BackEnd/LojaVirtualBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwss.studios.loja_virtual_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
