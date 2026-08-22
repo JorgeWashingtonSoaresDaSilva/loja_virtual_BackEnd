54c19c6b37d505d40911771ea2cec9c192b9da79
Jorge Washington Soares da Silva <jwss486@gmail.com>
2026-08-22 14:41:53 -0300

    Criado classe CategoriaProduto

--- src/main/java/com/jwss/studios/loja_virtual_BackEnd/model/CategoriaProduto.java
+++ src/main/java/com/jwss/studios/loja_virtual_BackEnd/model/CategoriaProduto.java
@@ -6,19 +6,19 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto",sequenceName ="seq_marca_produto" ,allocationSize = 1,initialValue = 1)
-public class MarcaProduto  implements Serializable {
 
+@Entity
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto",sequenceName ="seq_categoria_produto" ,allocationSize = 1,initialValue = 1)
+public class CategoriaProduto implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
     private Long id;
 
-    private  String nomeDesc;
+    private String nomeDesc;
 
     public Long getId() {
         return id;
@@ -39,7 +39,7 @@ public class MarcaProduto  implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        MarcaProduto that = (MarcaProduto) o;
+        CategoriaProduto that = (CategoriaProduto) o;
         return Objects.equals(id, that.id);
     }
 
